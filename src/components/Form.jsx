a7ac8181d18db85fb19731233bea178976ab27a7
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-01-18 21:34:46 +0500

    Changes made

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import Todo from "./Todo";
 
 function Form() {
   const inputText = useRef();
@@ -7,26 +8,56 @@ function Form() {
 
   const submitForm = (event) => {
     event.preventDefault();
-    
+    data.unshift({ todo: inputText.current.value });
+    setData([...data]);
+    inputText.current.value = "";
   };
 
+  console.log(data);
+
+  const deleteTodo = (index) => {
+    data.splice(index, 1);
+    setData([...data]);
+  };
+
+  const editTodo = (index)=>{
+    const newValue = prompt("Enter the new value:", data[index].todo);
+    data.splice(index, 1, {
+      todo: newValue,
+    });
+    setData([...data])
+  }
+
   return (
-    <div className="flex justify-center items-center mt-5">
-      <form onSubmit={submitForm}>
-        <input
-          type="text"
-          placeholder="Enter todo"
-          ref={inputText}
-          className="focus:outline-none border border-black p-2 rounded w-[300px]"
-        />
-        <button
-          type="submit"
-          className="bg-green-700 text-white p-2 ml-5 rounded"
-        >
-          Add
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="flex justify-center items-center mt-5">
+        <form onSubmit={submitForm}>
+          <input
+            type="text"
+            placeholder="Enter todo"
+            ref={inputText}
+            className="focus:outline-none border border-black p-2 rounded w-[300px]"
+          />
+          <button
+            type="submit"
+            className="bg-green-700 text-white p-2 ml-5 rounded"
+          >
+            Add
+          </button>
+        </form>
+      </div>
+      {data.map((item, index) => {
+        return (
+          <Todo
+            key={index}
+            todo={item.todo}
+            deleteTodo={deleteTodo}
+            index={index}
+            editTodo={editTodo}
+          />
+        );
+      })}
+    </>
   );
 }
 
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
-function Todo() {
+function Todo({ todo, deleteTodo, editTodo ,  index }) {
   return (
-    <div>Todo</div>
-  )
+    <div className="flex justify-between items-center ml-[430px] rounded-md mt-5 bg-white h-fit w-[400px] p-3">
+      <div>
+        <p className="text-[20px]">{todo}</p>
+      </div>
+      <div>
+        <button onClick={()=>{
+            editTodo(index)
+        }} className="p-2 bg-green-700 text-white rounded ml-2">
+          Edit
+        </button>
+
+        <button
+          onClick={() => {
+            deleteTodo(index);
+          }}
+          className="p-2 bg-red-600 text-white rounded ml-2"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Todo
+export default Todo;
